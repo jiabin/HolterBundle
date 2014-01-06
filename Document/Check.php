c60940c0cd418aabd50d7c3ccf180fb5bcbb583d
Eymen Gunay <eymen@egunay.com>
2014-01-06 12:54:17 +0100

    Doctrine mongodb id strategy changed to increment for check class

--- Document/Check.php
+++ Document/Check.php
@@ -11,7 +11,7 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 class Check extends BaseCheck
 {
     /**
-     * @ODM\Id
+     * @ODM\Id(strategy="INCREMENT")
      */    
     protected $id;
 
