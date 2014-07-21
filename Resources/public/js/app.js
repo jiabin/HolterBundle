90d771f4808be485e637b5700e2353c2559a5e07
Eymen Gunay <eymen@egunay.com>
2014-07-21 16:26:08 +0200

    Display group support added for checks

--- Resources/public/js/app.js
+++ Resources/public/js/app.js
@@ -9,6 +9,23 @@ HolterApp.controller('HolterCtrl', function($scope, $http) {
       alert('Houston, we have a problem');
       location.reload();
     }).then(function(res) {
+      // Put in groups
+      var groups = {};
+      for (i in res.data.checks) {
+        var check = res.data.checks[i];
+        var group = check.display_group;
+        if (group == undefined || groups == null) {
+          group = 'default';
+        }
+        if (groups[group] == undefined) {
+          groups[group] = {
+            name: group,
+            checks: []
+          };
+        }
+        groups[group].checks.push(check);
+      }
+      $scope.groups = groups;
       $scope.status = res.data;
       // Change favicon
       changeFavicon(res.data.status_name);
@@ -29,23 +46,4 @@ angular.module('HolterApp').filter('fromNow', function() {
   return function(date) {
     return moment(date).fromNow();
   }
-});
-
-HolterApp.filter('orderObjectBy', function(){
- return function(input, attribute) {
-    if (!angular.isObject(input)) return input;
-
-    console.debug(input);
-    var array = [];
-    for(var objectKey in input) {
-        array.push(input[objectKey]);
-    }
-
-    array.sort(function(a, b){
-        a = parseInt(a[attribute]);
-        b = parseInt(b[attribute]);
-        return a - b;
-    });
-    return array;
- }
 });
